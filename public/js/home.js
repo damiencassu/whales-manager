6e5a1676af8f77a1d4e93faf0f9a27e57d0ea693
damiencassu <damiencassu@users.noreply.github.com>
2023-03-26 13:23:17 +0200

    fix (frontend): fix container visual output when more than 3 containers

--- public/js/home.js
+++ public/js/home.js
@@ -6,19 +6,21 @@ async function getContainersList () {
 	var dataHtml = "<div class=\"row\">";
 	var elementsInRow = 0;
 	for (var index=0; index < data.length; index++) {
-		if (elementsInRow < 3) {
-			dataHtml+= "<div class=\"col wm-container\">";
-			dataHtml+= "<h3>" + data[index].name  + "</h3>";
-			dataHtml+= "<p>" + data[index].image + "</p>";
-			dataHtml+= "<p><span class=\"" + data[index].imageHtmlClass.htmlClass + " fa-2xl wm-container-icon\"></span></p>";
-			dataHtml+= "<p><span class=\"wm-container-status " + data[index].stateHtmlClass.htmlClass + "\">" + data[index].state.toUpperCase() + "</span></p>";
+		if (elementsInRow >= 3) {
 			dataHtml+= "</div>";
-			elementsInRow++;
-		} else {
-			dataHtml+= "</div>";
-			dataHtml = "<div class=\"row\">";
-			elementsInRow = 0;
+                        dataHtml += "<div class=\"row\">";
+                        elementsInRow = 0;
 		}
+		
+		dataHtml+= "<div class=\"col-3 wm-container\">";
+		dataHtml+= "<h3>" + data[index].name  + "</h3>";
+		dataHtml+= "<p>" + data[index].image + "</p>";
+		dataHtml+= "<p><span class=\"" + data[index].imageHtmlClass.htmlClass + " fa-2xl wm-container-icon\"></span></p>";
+		dataHtml+= "<p><span class=\"wm-container-status " + data[index].stateHtmlClass.htmlClass + "\">" + data[index].state.toUpperCase() + "</span></p>";
+		dataHtml+= "</div>";
+		elementsInRow++;
+		console.log(elementsInRow + ":" + dataHtml);
+		
 	}
 	dataHtml+= "</div>";	
 	document.getElementById("mainContent").innerHTML = dataHtml;
